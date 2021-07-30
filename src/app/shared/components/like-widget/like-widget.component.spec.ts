845b8544ae0f2b36441d5d9d910659844c94b5f5
Matheus B. Grigoletto <matheus.grigoletto@gmail.com>
2021-07-30 17:26:47 -0300

    Aula 04 - Testando componentes
    Nesta aula, aprendemos:
    
    - TestBed e criação de módulos de teste
    - Abordagens Test First / Test Later
    - Fixture de componentes
    - Teste de input e output properties
    - Asserções assíncronas
    - Introdução ao uso do Spy
    - Padronização na descrição de testes

--- src/app/shared/components/like-widget/like-widget.component.spec.ts
+++ src/app/shared/components/like-widget/like-widget.component.spec.ts
@@ -1,16 +1,22 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+// import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
+// import { UniqueIdService } from '../../services/unique-id/unique-id.service';
+import { LikeWidgetModule } from './like-widget.module';
 import { LikeWidgetComponent } from './like-widget.component';
 
-describe('LikeWidgetComponent', () => {
+describe(LikeWidgetComponent.name, () => {
   let component: LikeWidgetComponent;
   let fixture: ComponentFixture<LikeWidgetComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LikeWidgetComponent ]
-    })
-    .compileComponents();
+      // Test first
+      /*declarations: [LikeWidgetComponent],
+      imports: [FontAwesomeModule],
+      providers: [UniqueIdService],*/
+      imports: [LikeWidgetModule],
+    }).compileComponents();
   });
 
   beforeEach(() => {
@@ -19,7 +25,41 @@ describe('LikeWidgetComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it(`${LikeWidgetComponent.name} should be created`, () => {
     expect(component).toBeTruthy();
+  });
+
+  it(`Should auto generate ID during ngOnInit when (@Input id) is NOT assigned`, () => {
+    expect(component.id).toBeTruthy();
+  });
+
+  it(`Should NOT auto-generate ID during ngOnInit when (@Input id) is assigned`, () => {
+    const id = 'awesome';
+    component.id = id;
+
+    fixture.detectChanges();
+
+    expect(component.id).toBe(id);
+  });
+
+  // bazuca pra uma formiga
+  /*it(`#${LikeWidgetComponent.prototype.like.name}
+  should trigger event emitter when called`, (done) => {
+    fixture.detectChanges();
+
+    component.liked.subscribe(() => {
+      expect(true).toBeTrue();
+      done();
+    });
+
+    component.like();
+  });*/
+
+  it(`#${LikeWidgetComponent.prototype.like.name}
+    should trigger (@Output liked) when called`, () => {
+    spyOn(component.liked, 'emit');
+    fixture.detectChanges();
+    component.like();
+    expect(component.liked.emit).toHaveBeenCalled();
   });
 });
