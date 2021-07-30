6dd929f61e41f8d14ba84e6cba407ce2d09248a1
Matheus B. Grigoletto <matheus.grigoletto@gmail.com>
2021-07-30 14:34:11 -0300

    Aula 02 - Lapidando nossos testes Nesta aula, aprendemos:
    - A importância de testes precisos, legíveis e fáceis de manter
    - O papel da função beforeEach
    - Teste de exceções
    - Como testes ajudam na refatoração
    - Expectativas com contexto
    - A diferença entre as funções toBe, ToBeTrue e ToBeTruthy

--- src/app/shared/services/unique-id/unique-id.service.spec.ts
+++ src/app/shared/services/unique-id/unique-id.service.spec.ts
@@ -1,12 +1,64 @@
 import { UniqueIdService } from './unique-id.service';
 
-describe(UniqueIdService.name, () => {
-  it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name} should generate id when called with prefix`, () => {
-    const uniqueIdService = new UniqueIdService();
-    const id = uniqueIdService.generateUniqueIdWithPrefix('app');
-    expect(id).toContain('app-');
-  });
-});
-
 // <alguma coisa> should <fazer algo> when <determinadas condições estiverem presentes>
 // nome do metodo com #
+
+describe(UniqueIdService.name, () => {
+  let uniqueIdService: UniqueIdService = null;
+
+  // chamado antes de cada it()
+  beforeEach(() => {
+    uniqueIdService = new UniqueIdService();
+  });
+
+  it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name}
+  should generate id when called with prefix`, () => {
+    const id = uniqueIdService.generateUniqueIdWithPrefix('app');
+    expect(id.startsWith('app-')).toBeTrue();
+  });
+
+  /*it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name}
+  should not duplicate IDs when called multiple times`, () => {
+    const uniqueIdService = new UniqueIdService();
+    const id1 = uniqueIdService.generateUniqueIdWithPrefix('app');
+    const id2 = uniqueIdService.generateUniqueIdWithPrefix('app');
+    expect(id1).not.toBe(id2);
+  });*/
+
+  it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name}
+  should not duplicate IDs when called multiple times`, () => {
+    const ids = new Set();
+    const total = 100;
+
+    for (let i = 0; i < total; i++) {
+      const id = uniqueIdService.generateUniqueIdWithPrefix('app');
+      ids.add(id);
+    }
+
+    expect(ids.size).toBe(total);
+  });
+
+  it(`#${UniqueIdService.prototype.getNumberOfGeneratedIds.name}
+  should return the number of generated ids when called`, () => {
+    const ids = new Set();
+    const total = 10;
+
+    for (let i = 0; i < total; i++) {
+      const id = uniqueIdService.generateUniqueIdWithPrefix('app');
+      ids.add(id);
+    }
+
+    expect(uniqueIdService.getNumberOfGeneratedIds()).toBe(total);
+  });
+
+  it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name}
+  should throw error when called with empty prefix`, () => {
+    const emptyValues = [null, undefined, ''];
+
+    emptyValues.forEach((value) => {
+      expect(() => uniqueIdService.generateUniqueIdWithPrefix(value))
+        .withContext(`## Empty value => ${value} ##`)
+        .toThrowError();
+    });
+  });
+});
